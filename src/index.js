299df6580fb6e1d629212a7083f18d267152fb59
bondarev vlad <bondarev.vlad.01@gmail.com>
2024-07-12 14:44:35 +0300

    creatae config webpack

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import { sayHi } from "./module";
+import "./style.css"
+// import icon from "../public/react-fav.svg"
+
+
+sayHi()
+
+// const myIcon = new Image();
+// myIcon.src = Icon;
+
+// document.body.appendChild(myIcon);
