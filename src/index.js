2178a418cdcf0b5e74482af80cd2f5cab685e2b6
bondarev vlad <bondarev.vlad.01@gmail.com>
2024-11-09 12:08:49 +0300

    refactor: refactor

--- src/index.js
+++ src/index.js
@@ -1,11 +1,1 @@
-import { sayHi } from "./module";
-import "./style.css"
-// import icon from "../public/react-fav.svg"
-
-
-sayHi()
-
-// const myIcon = new Image();
-// myIcon.src = Icon;
-
-// document.body.appendChild(myIcon);
+import "./css/style.css"
